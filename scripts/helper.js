ab9c26257965339ec57db0c07f1a6b9c7c4a95ac
zhangshiliu <zhangshiliu@metled.com.cn>
2020-03-25 12:27:44 +0800

    Fix: 在首页点击分页，导航栏首页按钮不激活 Bug

--- scripts/helper.js
+++ scripts/helper.js
@@ -2,8 +2,16 @@
  * Hexo 扩展：辅助函数（Helper）
  * 补充复杂的业务逻辑
  * https://hexo.io/zh-cn/api/helper.html
- * 用法：<%= helper_test('xxx') %>
+ * eg 用法：<%= helper_test('xxx') %>
  */
 hexo.extend.helper.register('helper_test', function (character) {
     return character + '😁';
+});
+
+hexo.extend.helper.register('isInHomePaging', function (pagePath, route) {
+    if (pagePath.length > 5 && route === '/' ) {
+        return pagePath.slice(0, 5) === 'page/';
+    } else {
+        return false;
+    }
 });
