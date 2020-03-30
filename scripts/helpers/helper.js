a381f3b41918ced0604951d67504a0534510a9e2
XPoet <i@xpoet.cn>
2020-03-30 11:07:57 +0800

    fix: archive 归档页面排序错乱的 bug

--- scripts/helpers/helper.js
+++ scripts/helpers/helper.js
@@ -9,12 +9,13 @@ hexo.extend.helper.register('isInHomePaging', function (pagePath, route) {
 hexo.extend.helper.register('createNewArchivePosts', function (posts) {
     const postList = [], postYearList = [];
     posts.forEach(post => postYearList.push(post.date.year()));
-    Array.from(new Set(postYearList)).reverse().forEach(year => {
+    Array.from(new Set(postYearList)).forEach(year => {
         postList.push({
             year: year,
             postList: []
         })
     });
+    postList.sort((a, b) => b.year - a.year);
     postList.forEach(item => {
         posts.forEach(post => item.year === post.date.year() && item.postList.push(post))
     });
