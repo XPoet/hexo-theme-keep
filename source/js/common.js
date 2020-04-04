4af9bd73fd0257aef07985aca38626d17aca53bb
XPoet <i@xpoet.cn>
2020-04-05 00:42:07 +0800

    update: 优化回到顶部功能

--- source/js/common.js
+++ source/js/common.js
@@ -4,9 +4,10 @@ const pageTemplateContainer = document.querySelector('.page-template');
 const menuBar = document.querySelector('.menu-bar');
 const windowMask = document.querySelector('.window-mask');
 const headerHeight = header.getBoundingClientRect().height;
+const scrollToTopValue = document.querySelector('.scroll-to-top-value');
 
 window.addEventListener('scroll', function (_e) {
-    const scrollTop = document.body.scrollHeight || document.documentElement.scrollHeight;
+    const scrollTop = document.body.scrollTop || document.documentElement.scrollTop;
     if (!isHeaderShrink && scrollTop > headerHeight) {
         isHeaderShrink = true;
         header.classList.add('header-wrapper-shrink');
@@ -16,7 +17,15 @@ window.addEventListener('scroll', function (_e) {
         header.classList.remove('header-wrapper-shrink');
         pageTemplateContainer.classList.remove('page-top-shrink');
     }
+
+    // show scroll percent
+    const scrollHeight = document.body.scrollHeight || document.documentElement.scrollHeight;
+    const clientHeight = window.innerHeight || document.documentElement.clientHeight;
+    const percent = Math.round(scrollTop / (scrollHeight - clientHeight) * 100).toFixed(0);
+    scrollToTopValue.innerHTML = percent === '0' ? '' : percent + '%';
+
 });
+
 
 menuBar.addEventListener('click', function (_e) {
     header.classList.toggle('header-drawer-show');
--- source/js/scroll-to-top.js
+++ source/js/scroll-to-top.js
@@ -1,9 +1,8 @@
 const scrollToTop = document.querySelector('.scroll-to-top');
-const scrollToTopValue = document.querySelector('.scroll-to-top-value');
 scrollToTop.addEventListener('click', () => {
     let scrollTopTimer = setInterval(function () {
         let top = document.body.scrollTop || document.documentElement.scrollTop;
-        let speed = top / 5;
+        let speed = top / 4;
         if (document.body.scrollTop !== 0) {
             document.body.scrollTop -= speed;
         } else {
@@ -15,14 +14,8 @@ scrollToTop.addEventListener('click', () => {
     }, 30);
 });
 
-
-const scrollHeight = document.body.scrollHeight || document.documentElement.scrollHeight;
-const clientHeight = window.innerHeight || document.documentElement.clientHeight;
-window.addEventListener('scroll', function (_e) {
-    var validH = scrollHeight - clientHeight;
-    var scrollH = document.body.scrollTop || document.documentElement.scrollTop;
-    scrollToTopValue.innerHTML = Math.round(scrollH / validH * 100).toFixed(0) + '%';
-
-});
+if (document.body.scrollHeight <= window.document.documentElement.clientHeight) {
+    scrollToTop.style.display = 'none';
+}
 
 
