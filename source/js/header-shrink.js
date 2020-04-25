e39b3716959e795fc1e0feb16de409e501f616a7
XPoet <i@xpoet.cn>
2020-04-25 21:45:45 +0800

    refactor: fetch out 'back to top' module & modified its style

--- source/js/header-shrink.js
+++ source/js/header-shrink.js
@@ -3,7 +3,6 @@ const headerDom = document.querySelector('.header-wrapper');
 const menuBarDom = document.querySelector('.menu-bar');
 const windowMaskDom = document.querySelector('.window-mask');
 const scrollPercentDom = document.querySelector('.scroll-percent');
-const scrollToTopDom = document.querySelector('.scroll-to-top');
 
 let isHeaderShrink = false;
 const headerHeight = headerDom.getBoundingClientRect().height;
@@ -19,21 +18,6 @@ window.addEventListener('scroll', function (_e) {
         headerDom.classList.remove('header-wrapper-shrink');
         pageTemplateDom.classList.remove('page-top-shrink');
     }
-
-    // show scroll percent
-    const scrollHeight = document.body.scrollHeight || document.documentElement.scrollHeight;
-    const clientHeight = window.innerHeight || document.documentElement.clientHeight;
-    const percent = Math.round(scrollTop / (scrollHeight - clientHeight) * 100).toFixed(0);
-
-
-    if (percent === '0') {
-        scrollToTopDom.style.display = 'none';
-    } else {
-        scrollToTopDom.style.display = 'block';
-        scrollPercentDom.innerHTML = percent + '%';
-    }
-
-
 });
 
 
--- source/js/scroll-to-top.js
+++ source/js/scroll-to-top.js
@@ -1,14 +1,34 @@
-scrollToTopDom.addEventListener('click', () => {
-    let scrollTopTimer = setInterval(function () {
-        let top = document.body.scrollTop || document.documentElement.scrollTop;
-        let speed = top / 3;
-        if (document.body.scrollTop !== 0) {
-            document.body.scrollTop -= speed;
+(function () {
+    const scrollToTopDom = document.querySelector('.scroll-to-top');
+
+    scrollToTopDom.addEventListener('click', () => {
+        let scrollTopTimer = setInterval(function () {
+            let top = document.body.scrollTop || document.documentElement.scrollTop;
+            let speed = top / 2;
+            if (document.body.scrollTop !== 0) {
+                document.body.scrollTop -= speed;
+            } else {
+                document.documentElement.scrollTop -= speed;
+            }
+            if (top === 0) {
+                clearInterval(scrollTopTimer);
+            }
+        }, 30);
+    });
+
+    window.addEventListener('scroll', function (_e) {
+        // back to top
+        // show scroll percent
+        const scrollTop = document.body.scrollTop || document.documentElement.scrollTop;
+        const scrollHeight = document.body.scrollHeight || document.documentElement.scrollHeight;
+        const clientHeight = window.innerHeight || document.documentElement.clientHeight;
+        const percent = Math.round(scrollTop / (scrollHeight - clientHeight) * 100).toFixed(0);
+        if (percent === '0') {
+            scrollToTopDom.style.display = 'none';
         } else {
-            document.documentElement.scrollTop -= speed;
+            scrollToTopDom.style.display = 'block';
+            scrollPercentDom.innerHTML = parseInt(percent) < 10 ? `0${percent}` : percent;
         }
-        if (top === 0) {
-            clearInterval(scrollTopTimer);
-        }
-    }, 30);
-});
+    });
+
+})();
