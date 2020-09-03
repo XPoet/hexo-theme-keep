bb74e232a968bc857e80b60e9b72d8b10e6a6c45
XPoet <i@xpoet.cn>
2020-09-03 15:12:32 +0800

    feat: add page browsing progress at the top of header

--- source/js/scroll-to-top.js
+++ source/js/scroll-to-top.js
@@ -1,7 +1,8 @@
 (function () {
     const scrollToTopDom = document.querySelector('.scroll-to-top');
+    const headerProgressDom = document.querySelector('.header-progress');
 
-    scrollToTopDom.addEventListener('click', () => {
+    const backToTop = () => {
         let scrollTopTimer = setInterval(function () {
             let top = document.body.scrollTop || document.documentElement.scrollTop;
             let speed = top / 2;
@@ -14,20 +15,29 @@
                 clearInterval(scrollTopTimer);
             }
         }, 30);
+    };
+
+    scrollToTopDom.addEventListener('click', () => {
+        backToTop();
     });
 
     window.addEventListener('scroll', function (_e) {
-        // back to top
-        // show scroll percent
+        // back to top & show scroll percent
         const scrollTop = document.body.scrollTop || document.documentElement.scrollTop;
         const scrollHeight = document.body.scrollHeight || document.documentElement.scrollHeight;
         const clientHeight = window.innerHeight || document.documentElement.clientHeight;
         const percent = Math.round(scrollTop / (scrollHeight - clientHeight) * 100).toFixed(0);
+        const ProgressPercent = (scrollTop / (scrollHeight - clientHeight) * 100).toFixed(3);
+
         if (percent === '0') {
             scrollToTopDom.style.display = 'none';
+            headerProgressDom.style.display = 'none';
         } else {
             scrollToTopDom.style.display = 'block';
-            scrollPercentDom.innerHTML = parseInt(percent) < 10 ? `0${percent}` : percent;
+            // scrollPercentDom.innerHTML = parseInt(percent) < 10 ? `0${percent}` : percent;
+
+            headerProgressDom.style.display = 'block';
+            headerProgressDom.style.width = `${ProgressPercent}%`;
         }
     });
 
