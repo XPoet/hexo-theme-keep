145272ff15e6d7c3f1542d563e4e38ed303e6947
XPoet <i@xpoet.cn>
2020-09-11 15:06:38 +0800

    pref: perfect configuration & optimize style

--- source/js/header-shrink.js
+++ source/js/header-shrink.js
@@ -1,41 +1,36 @@
 const pageTemplateDom = document.querySelector('.page-main-content');
-const articleTocContainerDom = document.querySelector('.article-toc-container');
+const sidebarToolsDom = document.querySelector('.sidebar-tools');
 const headerDom = document.querySelector('.header-wrapper');
 const menuBarDom = document.querySelector('.menu-bar');
 const windowMaskDom = document.querySelector('.window-mask');
-const scrollPercentDom = document.querySelector('.scroll-percent');
+
 
 let isHeaderShrink = false;
 const headerHeight = headerDom.getBoundingClientRect().height;
 
 window.addEventListener('scroll', function (_e) {
-    const scrollTop = document.body.scrollTop || document.documentElement.scrollTop;
-    if (!isHeaderShrink && scrollTop > headerHeight) {
-        isHeaderShrink = true;
-        headerDom.classList.add('header-wrapper-shrink');
-        pageTemplateDom.classList.add('page-main-content-top-shrink');
+  const scrollTop = document.body.scrollTop || document.documentElement.scrollTop;
+  if (!isHeaderShrink && scrollTop > headerHeight) {
+    isHeaderShrink = true;
+    headerDom.classList.add('header-wrapper-shrink');
+    pageTemplateDom.classList.add('page-main-content-top-shrink');
+    sidebarToolsDom.classList.add('sidebar-tools-shrink');
 
-        if (articleTocContainerDom) {
-            articleTocContainerDom.classList.add('article-toc-container-shrink');
-        }
-    } else if (isHeaderShrink && scrollTop <= headerHeight) {
-        isHeaderShrink = false;
-        headerDom.classList.remove('header-wrapper-shrink');
-        pageTemplateDom.classList.remove('page-main-content-top-shrink');
-
-        if (articleTocContainerDom) {
-            articleTocContainerDom.classList.remove('article-toc-container-shrink');
-        }
-    }
+  } else if (isHeaderShrink && scrollTop <= headerHeight) {
+    isHeaderShrink = false;
+    headerDom.classList.remove('header-wrapper-shrink');
+    pageTemplateDom.classList.remove('page-main-content-top-shrink');
+    sidebarToolsDom.classList.remove('sidebar-tools-shrink');
+  }
 });
 
 
 menuBarDom.addEventListener('click', function (_e) {
-    headerDom.classList.toggle('header-drawer-show');
+  headerDom.classList.toggle('header-drawer-show');
 });
 
 windowMaskDom.addEventListener('click', function (_e) {
-    headerDom.classList.toggle('header-drawer-show');
+  headerDom.classList.toggle('header-drawer-show');
 });
 
 
