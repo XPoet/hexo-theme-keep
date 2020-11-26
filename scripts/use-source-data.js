144f8b300dab00bab1bb4c24f86c6e0bdac98c84
XPoet <i@xpoet.cn>
2020-11-26 16:09:09 +0800

    feat: use _data yaml file to manage friends link

--- scripts/use-source-data.js
+++ scripts/use-source-data.js
@@ -1,10 +1,26 @@
 hexo.on('generateBefore', function () {
-    const rootConfig = hexo.config;
-    if (hexo.locals.get) {
-        const data = hexo.locals.get('data');
-        if (data && data._config) {
-            hexo.theme.config = data._config;
-        }
+
+  if (hexo.locals.get) {
+    const data = hexo.locals.get('data');
+
+    if (data) {
+
+      // theme config file handle
+      if (data._config) {
+        hexo.theme.config = data._config;
+
+      } else if (data.keep) {
+        hexo.theme.config = data.keep;
+
+      } else if (data._keep) {
+        hexo.theme.config = data._keep;
+      }
+
+      // friends link file handle
+      if (data.links || data.link) {
+        hexo.theme.config.links = (data.links || data.link);
+      }
+
     }
-    hexo.theme.config.rootConfig = rootConfig;
+  }
 });
