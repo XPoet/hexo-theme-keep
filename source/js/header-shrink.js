0ea7753fe758c1b03293c61c68b0c27a253d3ef0
XPoet <i@xpoet.cn>
2021-01-05 11:48:58 +0800

    refactor: header code refactor

--- source/js/header-shrink.js
+++ source/js/header-shrink.js
@@ -1,47 +1,34 @@
 KEEP.initHeaderShrink = () => {
   KEEP.utils.headerShrink = {
-
-    pageTemplateDom: document.querySelector('.page-main-content'),
     headerDom: document.querySelector('.header-wrapper'),
-    sidebarToolsDom: document.querySelector('.sidebar-tools'),
     isHeaderShrink: false,
-    isShowHeaderDrawer: false,
 
     init() {
       this.headerHeight = this.headerDom.getBoundingClientRect().height;
     },
 
     headerShrink() {
-
       const scrollTop = document.body.scrollTop || document.documentElement.scrollTop;
 
       if (!this.isHeaderShrink && scrollTop > this.headerHeight) {
         this.isHeaderShrink = true;
-        this.headerDom.classList.add('header-wrapper-shrink');
-        this.pageTemplateDom.classList.add('page-main-content-top-shrink');
-        this.sidebarToolsDom && this.sidebarToolsDom.classList.add('shrink');
+        document.body.classList.add('header-shrink');
       } else if (this.isHeaderShrink && scrollTop <= this.headerHeight) {
         this.isHeaderShrink = false;
-        this.headerDom.classList.remove('header-wrapper-shrink');
-        this.pageTemplateDom.classList.remove('page-main-content-top-shrink');
-        this.sidebarToolsDom && this.sidebarToolsDom.classList.remove('shrink');
+        document.body.classList.remove('header-shrink');
       }
 
     },
 
     initMenuBarButton() {
       document.querySelector('.menu-bar').addEventListener('click', () => {
-        this.isShowHeaderDrawer = !this.isShowHeaderDrawer;
-        document.body.style.overflow = this.isShowHeaderDrawer ? 'hidden' : 'auto';
-        this.headerDom.classList.toggle('header-drawer-show');
+        document.body.classList.toggle('header-drawer-show');
       });
     },
 
     initWindowMask() {
       document.querySelector('.window-mask').addEventListener('click', () => {
-        this.isShowHeaderDrawer = !this.isShowHeaderDrawer;
-        document.body.style.overflow = this.isShowHeaderDrawer ? 'hidden' : 'auto';
-        this.headerDom.classList.toggle('header-drawer-show');
+        document.body.classList.toggle('header-drawer-show');
       });
     },
   }
