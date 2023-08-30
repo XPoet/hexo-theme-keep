50cd62e5a5455b2609f4b6ce6fc60d82595b6d64
XPoet <i@xpoet.cn>
2023-08-30 22:12:55 +0800

    build: add commit lint

---
+++ .commitlintrc.js
@@ -0,0 +1,25 @@
+module.exports = {
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'type-enum': [
+      2,
+      'always',
+      [
+        'feat',
+        'fix',
+        'docs',
+        'style',
+        'refactor',
+        'perf',
+        'test',
+        'build',
+        'ci',
+        'chore',
+        'revert',
+        'WIP',
+        'ui',
+        'release'
+      ]
+    ]
+  }
+}
