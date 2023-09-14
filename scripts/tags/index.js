2363829c069d1b7fd7fc3278720ffde6bd9dad9a
XPoet <i@xpoet.cn>
2023-09-14 22:42:27 +0800

    feat: add custom button tag in post

--- scripts/tags/index.js
+++ scripts/tags/index.js
@@ -5,3 +5,9 @@
 const noteTag = require('./note')(hexo)
 hexo.extend.tag.register('note', noteTag, true)
 hexo.extend.tag.register('keep-note', noteTag, true)
+
+const buttonTag = require('./button')(hexo)
+hexo.extend.tag.register('button', buttonTag, true)
+hexo.extend.tag.register('btn', buttonTag, true)
+hexo.extend.tag.register('keep-button', buttonTag, true)
+hexo.extend.tag.register('keep-btn', buttonTag, true)
