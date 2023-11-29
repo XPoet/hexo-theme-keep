5a099467f33ebd548dd712ad80bc0e5af3044de2
XPoet <i@xpoet.cn>
2023-11-29 21:42:13 +0800

    perf: optimize img src url generation

--- scripts/filters/image-handle.js
+++ scripts/filters/image-handle.js
@@ -10,35 +10,43 @@ const isAddRootPath = (root, src) => {
   }
 }
 
+const regExpImg = /<img([^>]*)src="([^"]*)"([^>\/]*)\/?\s*>/gim
+
+hexo.extend.filter.register(
+  'before_post_render',
+  function (data) {
+    const theme = hexo.theme.config
+    data.content = data.content.replace(regExpImg, function (match, attrBegin, src, attrEnd) {
+      if (!src) return match
+      return `<img ${attrBegin} src="${isAddRootPath(theme.root, src)}" ${attrEnd}>`
+    })
+  },
+  1
+)
+
 hexo.extend.filter.register(
   'after_post_render',
   function (data) {
     const theme = hexo.theme.config
 
-    data.content = data.content.replace(
-      /<img([^>]*)src="([^"]*)"([^>\/]*)\/?\s*>/gim,
-      function (match, attrBegin, src, attrEnd) {
-        if (!src) return match
-        // image lazy load
-        if (theme?.lazyload?.enable === true) {
-          let hasAlt = false
-          if (attrBegin.includes('alt="')) {
-            hasAlt = true
-          }
-          return `<img ${attrBegin}
+    data.content = data.content.replace(regExpImg, function (match, attrBegin, src, attrEnd) {
+      if (!src) return match
+      // image lazy load
+      if (theme?.lazyload?.enable === true) {
+        let hasAlt = false
+        if (attrBegin.includes('alt="')) {
+          hasAlt = true
+        }
+        return `<img ${attrBegin}
                        lazyload
                        ${hasAlt ? '' : 'alt="image"'}
-                       data-src="${isAddRootPath(theme.root, src)}"
+                       data-src="${src}"
                        ${attrEnd}
                  >`
-        } else {
-          return `<img ${attrBegin}
-                       src="${isAddRootPath(theme.root, src)}"
-                       ${attrEnd}
-                 >`
-        }
+      } else {
+        return `<img ${attrBegin} src="${src}" ${attrEnd}>`
       }
-    )
+    })
   },
   1
 )
