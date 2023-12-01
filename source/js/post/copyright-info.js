3ed9c5659f36ff920856bf93b17f29e2bdeac2a7
XPoet <i@xpoet.cn>
2023-12-01 14:35:38 +0800

    ui: optimize copyright info style

--- source/js/post/copyright-info.js
+++ source/js/post/copyright-info.js
@@ -4,7 +4,9 @@ function initCopyrightInfoHelper() {
   KEEP.utils.copyrightInfoHelper = {
     // set post link
     initSetPostLink() {
-      const postLinkContentDom = document.querySelector('.copyright-info-content .post-link')
+      const postLinkContentDom = document.querySelector(
+        '.copyright-info-content .copyright-post-link'
+      )
       postLinkContentDom && (postLinkContentDom.innerHTML = decodeURI(window.location.href))
     },
 
@@ -36,8 +38,8 @@ function initCopyrightInfoHelper() {
 
       copyDom.addEventListener('click', () => {
         if (!isCopied) {
-          const author = cicDom.querySelector('.post-author .content').innerHTML
-          const link = cicDom.querySelector('.post-link').innerHTML
+          const author = cicDom.querySelector('.copyright-post-author .content').innerHTML
+          const link = cicDom.querySelector('.copyright-post-link').innerHTML
           const tgtTxt = `${ccLang.author}${colon}${author}\n${ccLang.link}${colon}${link}`
           navigator.clipboard.writeText(tgtTxt).then(() => {
             setCopyDomContent('fa-copy', 'fa-check', ccLang.copied, true)
