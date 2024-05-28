5e31bef1ac59def3418a49b59ffe170babb3814c
XPoet <i@xpoet.cn>
2024-05-28 16:51:03 +0800

    perf: supports configuring initial theme mode (#327)

--- source/js/toggle-theme.js
+++ source/js/toggle-theme.js
@@ -26,6 +26,18 @@ KEEP.initModeToggle = () => {
     },
 
     initModeStatus() {
+      const configMode = KEEP.theme_config?.base_info?.mode
+
+      if (configMode === 'dark') {
+        this.enableDarkMode()
+        return
+      }
+
+      if (configMode === 'light') {
+        this.enableLightMode()
+        return
+      }
+
       const styleStatus = KEEP.getStyleStatus()
 
       if (styleStatus) {
