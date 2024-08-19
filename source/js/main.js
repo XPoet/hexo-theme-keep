5dede083c1976f385fe371b0f0e9c2a577207512
XPoet <i@xpoet.cn>
2024-08-19 10:27:55 +0800

    fix: fix the error caused by closing lazy loading of images (#365)

--- source/js/main.js
+++ source/js/main.js
@@ -1,7 +1,7 @@
 /* global KEEP */
 
 window.addEventListener('DOMContentLoaded', () => {
-  const { version, local_search } = KEEP.theme_config
+  const { version, local_search, lazyload } = KEEP.theme_config
 
   KEEP.themeInfo = {
     theme: `Keep v${version}`,
@@ -69,7 +69,10 @@ window.addEventListener('DOMContentLoaded', () => {
     KEEP.initBack2Top()
     KEEP.initCodeBlock()
     KEEP.setFooterVersion()
-    KEEP.initLazyLoad()
+
+    if (lazyload?.enable === true) {
+      KEEP.initLazyLoad()
+    }
 
     if (local_search?.enable === true) {
       KEEP.initLocalSearch()
