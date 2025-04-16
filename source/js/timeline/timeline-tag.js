172c0d073127777d4da968893f4ad88ff273a614
Enoch <cheers0606520@gmail.com>
2025-04-16 23:27:10 +0800

    Update timeline-tag.js

--- source/js/timeline/timeline-tag.js
+++ source/js/timeline/timeline-tag.js
@@ -1,7 +1,7 @@
 /**
  * 时间轴标签插件
  * 
- * 用于在文章中创建时间轴
+ * 用于在文章中创建时间轴。 需要安装marked：  ` npm install marked@4.0.0 --save `
  * 
  * 使用方法:
  * {% timeline %}
@@ -15,36 +15,40 @@
  * {% endtimeline %}
  */
 
+
 'use strict';
 
-function timeline(args, content) {
-  const timeline = content
-    .replace(/^\s*/, '')
-    .replace(/\s*$/, '');
+const marked = require('marked');
 
-  return `<div class="keep-timeline">${timeline}</div>`;
-}
+// 设置marked选项
+marked.setOptions({
+  breaks: true,
+  smartLists: true,
+  gfm: true
+});
 
-function timenode(args, content) {
+// 主时间轴标签
+hexo.extend.tag.register('timeline', function(args, content) {
+  return `<div class="keep-timeline">${content}</div>`;
+}, { ends: true });
+
+// 时间节点标签
+hexo.extend.tag.register('timenode', function(args, content) {
   const date = args[0] || '';
-  const text = content
-    .replace(/<p>|<\/p>/g, '')
-    .replace(/^\s*/, '')
-    .replace(/\s*$/, '');
-
+  
+  // 使用marked渲染Markdown内容
+  const renderedContent = marked.parse(content);
+  
   return `
     <div class="keep-timeline-item">
       <div class="keep-timeline-date">${date}</div>
       <div class="keep-timeline-dot"></div>
       <div class="keep-timeline-content">
         <div class="keep-timeline-card">
-          ${text}
+          ${renderedContent}
         </div>
       </div>
     </div>
   `;
-}
+}, { ends: true });
 
-// 注册标签
-hexo.extend.tag.register('timeline', timeline, { ends: true });
-hexo.extend.tag.register('timenode', timenode, { ends: true });
